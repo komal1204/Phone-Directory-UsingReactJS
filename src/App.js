744a74c4102b6db6fe1361bc4367abfc40bf0cce
komal1204 <kattykomal1@gmail.com>
2019-02-18 16:31:06 +0530

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+import Header from "./Header"
 
 class App extends Component {
   render() {
+    //let x=10;
     return (
       <div>
-      <div className="header">
-        Phone Directory
-      </div>
+          <Header></Header>
       <button>Add</button>
       <div>
         <span>Name</span><br/>
         <span>Phone</span>
+       {/* <span>{x*2}</span> */}
       </div>
       {/* <label htmlFor="name">Name</label>
       <input id = "name" type="text" placeholder="type Here" defaultValue="Komal"></input> */}
---
+++ src/Header.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Header = function(){
+    return(
+        <div className="header">
+        Phone Directory
+      </div>
+    )
+}
+
+// class Header extends Component{
+//     render(){
+//         <div className="header">
+//                  Phone Directory
+//                </div>
+//     }
+// }
+
+export default Header;
